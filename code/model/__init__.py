ed7ab49230f2ff80f6f3ffc6e50d60e6a05c85cc
wuzhi456 <793422303@qq.com>
2025-12-16 17:00:08 +0800

    final

--- code/model/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-Model package for Adult Census Income Classification
-"""
-from .neural_network import NeuralNetwork, create_model
-
-__all__ = ['NeuralNetwork', 'create_model']
--- code/train/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Training package for Adult Census Income Classification
-"""
-from .data_preprocessing import DataPreprocessor
-from .trainer import Trainer
-
-__all__ = ['DataPreprocessor', 'Trainer']
